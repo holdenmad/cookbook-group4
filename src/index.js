e01c1bc326115283fe7f259edccaebc1f9859786
Marvelous-Maddie <65891259+Marvelous-Maddie@users.noreply.github.com>
2020-06-11 17:58:05 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+document.getElementById("random-recipe").addEventListener("click", function() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 1:
+      document.location =
+        "https://holdenmad.github.io/cookbook-group4/recipe-page.html";
+      break;
+    case 2:
+      document.location =
+        "https://holdenmad.github.io/cookbook-group4/recipe-page2.html";
+      break;
+    default:
+      document.location =
+        "https://holdenmad.github.io/cookbook-group4/recipe-page3.html";
+  }
+});
