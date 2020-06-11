ae0d3fa215fdd6d2719f9b3184e388607ae11ab4
holdenmad <holden.madagame@gmail.com>
2020-06-11 17:51:21 +0200

    correctly linked index.js to recipe-page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "./styles.css";
+//import "./styles.css";
+
+const shareButton = document.querySelector("#share-button");
+const websitePark = "https://park.org/";
+
+document.querySelector("#share-button").addEventListener("click", function () {
+    document.location = "https://www.park.org/";
+    alert(`Taking you to: ${websitePark}`);
+    console.log("Shared!")
+});
+
+console.log(document.querySelector("#share-button"));
