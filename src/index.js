28e83a8b47878d3fc6ce0d5e98d3890413eadaae
NiklasL1 <66204186+NiklasL1@users.noreply.github.com>
2020-06-12 17:31:56 +0200

    trying to fix js on githubpages try2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-const randomRecipe = document.getElementById("random-recipe");
+window.onload = function() {
+  const randomRecipe = document.getElementById("random-recipe");
 
-console.log(randomRecipe);
+  console.log(randomRecipe);
 
-if (randomRecipe) {
-  randomRecipe.addEventListener("click", function() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-      case 1:
-        document.location = "recipe-page.html";
-        break;
-      case 2:
-        document.location = "recipe-page2.html";
-        break;
-      default:
-        document.location = "recipe-page3.html";
-    }
-  });
-}
+  if (randomRecipe) {
+    randomRecipe.addEventListener("click", function() {
+      let randomNumber = Math.floor(Math.random() * 3);
+      switch (randomNumber) {
+        case 1:
+          document.location = "recipe-page.html";
+          break;
+        case 2:
+          document.location = "recipe-page2.html";
+          break;
+        default:
+          document.location = "recipe-page3.html";
+      }
+    });
+  }
 
-//Recipe-Page Share Button
+  //Recipe-Page Share Button
 
-const shareButton = document.getElementById("share-button");
+  const shareButton = document.getElementById("share-button");
 
-console.log(shareButton);
+  console.log(shareButton);
 
-if (shareButton) {
-  shareButton.addEventListener("click", function() {
-    alert("Shared! Thanks for sharing. :)");
-    console.log("Shared!");
-  });
-}
+  if (shareButton) {
+    shareButton.addEventListener("click", function() {
+      alert("Shared! Thanks for sharing. :)");
+      console.log("Shared!");
+    });
+  }
+};
