1bc3265e387c0fc69883610cfd9d41beebb80c1a
Marvelous-Maddie <65891259+Marvelous-Maddie@users.noreply.github.com>
2020-06-12 09:47:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 document.getElementById("random-recipe").addEventListener("click", function() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
